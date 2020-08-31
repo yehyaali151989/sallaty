8de078a22fbd1132bf791f6c5a672a9110a95933
Yehya <yehyaali151989@gmail.com>
2020-08-31 05:43:49 +0200

    edit admin profile

--- resources/views/admin/pages/profile/edit.blade.php
+++ resources/views/admin/pages/profile/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.dashboard')
 
 @section('title')
-    {{__('mine.Edit Shipping Methods | Sallaty')}}
+    {{__('mine.Edit Profile | Sallaty')}}
 @endsection
 
 @section('content')
@@ -16,7 +16,7 @@
                                     <a href="{{ route('admin.dashboard') }}">{{ __('mine.Home') }}</a>
                                 </li>
                                 <li class="breadcrumb-item active">
-                                    {{ __('mine.Shipping Methods') }}
+                                    {{ __('mine.Edit Profile') }}
                                 </li>
                             </ol>
                         </div>
@@ -29,7 +29,7 @@
                         <div class="col-md-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h4 class="card-title" id="basic-layout-form">{{ __('mine.Edit Shipping Methods') }}</h4>
+                                    <h4 class="card-title" id="basic-layout-form">{{ __('mine.Edit Profile') }}</h4>
                                     <a class="heading-elements-toggle">
                                         <i class="la la-ellipsis-v font-medium-3"></i>
                                     </a>
@@ -46,17 +46,17 @@
                                 @include('admin.includes.alerts.errors')
                                 <div class="card-content collapse show">
                                     <div class="card-body">
-                                        <form class="form" action="{{ route('update.shippings.methods', $shippingMethod->id) }}" method="POST" enctype="multipart/form-data">
+                                        <form class="form" action="{{ route('update.profile.admin') }}" method="POST" enctype="multipart/form-data">
                                             @csrf
                                             @method('PUT')
-                                            <input type="hidden" name="id" value="{{$shippingMethod->id}}">
+                                            <input type="hidden" name="id" value="{{$admin->id}}">
                                             <div class="form-body">
                                                 <div class="row">
                                                     <div class="col-md-6">
                                                         <div class="form-group">
-                                                            <label for="projectinput1">{{ __('mine.Name') }}</label>
-                                                            <input type="text" value="{{$shippingMethod->value}}" id="value" class="@error('value') is-invalid @enderror form-control" name="value">
-                                                            @error("value")
+                                                            <label for="name">{{ __('mine.Name') }}</label>
+                                                            <input type="text" value="{{$admin->name}}" id="name" class="@error('name') is-invalid @enderror form-control" name="name">
+                                                            @error("name")
                                                             <span class="text-danger">{{$message}}</span>
                                                             @enderror
                                                         </div>
@@ -64,9 +64,29 @@
 
                                                     <div class="col-md-6">
                                                         <div class="form-group">
-                                                            <label for="projectinput1">{{ __('mine.Shpping Cost') }}</label>
-                                                            <input type="number" value="{{$shippingMethod->plain_value}}" id="plain_value" class="@error('plain_value') is-invalid @enderror form-control" name="plain_value">
-                                                            @error("plain_value")
+                                                            <label for="email">{{ __('mine.Email') }}</label>
+                                                            <input type="text" value="{{$admin->email}}" id="email" class="@error('email') is-invalid @enderror form-control" name="email">
+                                                            @error("email")
+                                                            <span class="text-danger">{{$message}}</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="password">{{ __('mine.New Password') }}</label>
+                                                            <input type="password" value="" id="password" class="@error('password') is-invalid @enderror form-control" name="password">
+                                                            @error("password")
+                                                            <span class="text-danger">{{$message}}</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="password_confirmation">{{ __('mine.Confirm New Password') }}</label>
+                                                            <input type="password" value="" id="password_confirmation" class="@error('password_confirmation') is-invalid @enderror form-control" name="password_confirmation">
+                                                            @error("password_confirmation")
                                                             <span class="text-danger">{{$message}}</span>
                                                             @enderror
                                                         </div>
